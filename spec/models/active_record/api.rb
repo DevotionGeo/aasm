5c0e0866b2972eecbdc83db7ba1cb97d192b500d
Thorsten Böttger <boettger@mt7.de>
2015-05-05 22:16:25 +1200

    using default name (all tests green)

--- spec/models/active_record/api.rb
+++ spec/models/active_record/api.rb
@@ -23,15 +23,15 @@ class ProvidedState
     end
   end
 
-  def aasm_read_state
+  def aasm_read_state(*args)
     :beta
   end
 
-  def aasm_write_state(new_state)
+  def aasm_write_state(new_state, *args)
     @persisted_store = new_state
   end
 
-  def aasm_write_state_without_persistence(new_state)
+  def aasm_write_state_without_persistence(new_state, *args)
     @transient_store = new_state
   end
 end
@@ -61,15 +61,15 @@ class ProvidedAndPersistedState < ActiveRecord::Base
     end
   end
 
-  def aasm_read_state
+  def aasm_read_state(*args)
     :gamma
   end
 
-  def aasm_write_state(new_state)
+  def aasm_write_state(new_state, *args)
     @persisted_store = new_state
   end
 
-  def aasm_write_state_without_persistence(new_state)
+  def aasm_write_state_without_persistence(new_state, *args)
     @transient_store = new_state
   end
 end
